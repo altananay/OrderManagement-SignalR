e9381e128aefb1f5aea1e8f11d3252ec6907d55c
altananay <altananay@outlook.com>
2024-01-02 01:45:35 +0300

    Added signalR connection between front-end and back-end.

--- Backend/WebAPI/Hubs/SignalRHub.cs
+++ Backend/WebAPI/Hubs/SignalRHub.cs
@@ -1,8 +1,13 @@
 ﻿using Microsoft.AspNetCore.SignalR;
+using Persistence.Contexts;
 
 namespace WebAPI.Hubs;
 
-public class SignalRHub : Hub
+public class SignalRHub(SignalRContext _context) : Hub
 {
-
+    public async Task SendCategoryCount()
+    {
+        var value = _context.Categories.Count();
+        await Clients.All.SendAsync("ReceiveCategoryCount", value);
+    }
 }
