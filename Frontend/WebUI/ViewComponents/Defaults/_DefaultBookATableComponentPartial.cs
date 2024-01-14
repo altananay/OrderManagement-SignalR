97a30b63eb9d25cfdd84e253a83c56eab8e47654
altananay <altananay@outlook.com>
2024-01-15 00:44:15 +0300

    Front-end ui development - 2

---
+++ Frontend/WebUI/ViewComponents/Defaults/_DefaultBookATableComponentPartial.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebUI.ViewComponents.Defaults;
+
+public class _DefaultBookATableComponentPartial : ViewComponent
+{
+    public IViewComponentResult Invoke()
+    {
+        return View();
+    }
+}
