26ad38ecf865af3391b9743f38fd86f2614877d7
altananay <altananay@outlook.com>
2024-01-22 00:41:29 +0300

    Menu Controller, View Component and View added. AJAX method added.

---
+++ Frontend/WebUI/Controllers/MenusController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Net.Http;
+using WebUI.Dtos.Responses.Product;
+
+namespace WebUI.Controllers;
+
+public class MenusController(IHttpClientFactory _httpClientFactory, IConfiguration _configuration) : Controller
+{
+    public async Task<IActionResult> Index()
+    {
+        var client = _httpClientFactory.CreateClient();
+        var responseMessage = await client.GetAsync(_configuration.GetValue<string>("Endpoints:GetAllProducts"));
+        var jsonData = await responseMessage.Content.ReadAsStringAsync();
+        var values = JsonConvert.DeserializeObject<List<GetAllProductsWithCategoryResponse>>(jsonData);
+        return View(values);
+    }
+}
